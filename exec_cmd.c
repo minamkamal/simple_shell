44947f5631e5f57ad913bb8b8132c085c0027209
Mina <mina.m.kamal@gmail.com>
2023-08-25 09:44:47 +0300

    fixed memory leake in my_getline function

--- exec_cmd.c
+++ exec_cmd.c
@@ -6,39 +6,39 @@
  */
 void exec_cmd(char **str_arr)
 {
-    char *cmd = NULL;
-    char *finalCmd = NULL;
-    int status;
-    pid_t pid;
+	char *cmd = NULL;
+	char *finalCmd = NULL;
+	int status;
+	pid_t pid;
 
-    if (str_arr)
-    {
-        cmd = str_arr[0];
-        finalCmd = get_path(cmd);
+	if (str_arr)
+	{
+		cmd = str_arr[0];
+		finalCmd = get_path(cmd);
 
-        if (finalCmd != NULL)
-        {
-            pid = fork();
-            if (pid == -1)
-            {
-                perror("fork error");
-            }
-            else if (pid == 0)
-            {
-                if (execve(finalCmd, str_arr, NULL) == -1)
-                {
-                    perror("execve error");
-                    exit(1);
-                }
-            }
-            else
-            {
-                wait(&status);
-            }
-        }
-        else
-        {
-            printf("bash: %s: not found\n", cmd);
-        }
-    }
+		if (finalCmd != NULL)
+		{
+			pid = fork();
+			if (pid == -1)
+			{
+				perror("fork error");
+			}
+			else if (pid == 0)
+			{
+				if (execve(finalCmd, str_arr, NULL) == -1)
+				{
+					perror("execve error");
+					exit(1);
+				}
+			}
+			else
+			{
+				wait(&status);
+			}
+		}
+		else
+		{
+			printf("bash: %s: not found\n", cmd);
+		}
+	}
 }
