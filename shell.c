7facf1857909d91d4dad0c87c4a00f9a2b8c925d
Mina <mina.m.kamal@gmail.com>
2023-11-16 01:12:39 +0200

    fixed gcc

--- shell.c
+++ shell.c
@@ -11,14 +11,24 @@ int main(void)
 	size_t n = 0;
 	ssize_t read_input;
 
-	if (isatty(STDIN_FILENO))
+	while (1)
 	{
-		printf("%s", user_prompt);
-		fflush(stdout);
-	}
-	while ((read_input = getline(&lineptr, &n, stdin)) != -1)
-	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("%s", user_prompt);
+			fflush(stdout);
+		}
+		read_input = getline(&lineptr, &n, stdin);
 		if (read_input <= 1)
+		{
+			continue;
+		} else if (strcmp(lineptr, "exit\n") == 0)
+		{
+			printf("\n");
+			break;
+		}
+		str_arr = tokenize_input(lineptr);
+		if (str_arr == NULL)
 		{
 			if (isatty(STDIN_FILENO))
 			{
@@ -27,20 +37,8 @@ int main(void)
 			}
 			continue;
 		}
-		else if (strcmp(lineptr, "exit\n") == 0)
-		{
-			printf("\n");
-			break;
-		}
-		str_arr = tokenize_input(lineptr);
 		exec_cmd(str_arr);
 		free_tokens(str_arr);
-
-		if (isatty(STDIN_FILENO))
-		{
-			printf("%s", user_prompt);
-			fflush(stdout);
-		}
 	}
 	free(lineptr);
 	return (0);
