a517563a0b26918f59b23749c9d5f84e9ee3cdca
Mina <mina.m.kamal@gmail.com>
2023-11-16 02:21:06 +0200

    shell

--- shell.c
+++ shell.c
@@ -11,28 +11,14 @@ int main(void)
 	size_t n = 0;
 	ssize_t read_input;
 
-	while (1)
+	if (isatty(STDIN_FILENO))
 	{
-		if (isatty(STDIN_FILENO))
-		{
-			printf("%s", user_prompt);
-			fflush(stdout);
-		}
-		read_input = getline(&lineptr, &n, stdin);
-		if (read_input == -1)
-		{
-			printf("\n");
-			break;
-		} else if (read_input <= 1)
-		{
-			continue;
-		} else if (strcmp(lineptr, "exit\n") == 0)
-		{
-			printf("\n");
-			break;
-		}
-		str_arr = tokenize_input(lineptr);
-		if (str_arr == NULL)
+		printf("%s", user_prompt);
+		fflush(stdout);
+	}
+	while ((read_input = getline(&lineptr, &n, stdin)) != -1)
+	{
+		if (read_input <= 1)
 		{
 			if (isatty(STDIN_FILENO))
 			{
@@ -41,8 +27,19 @@ int main(void)
 			}
 			continue;
 		}
+		else if (strcmp(lineptr, "exit\n") == 0)
+		{
+			printf("\n");
+			break;
+		}
+		str_arr = tokenize_input(lineptr);
 		exec_cmd(str_arr);
 		free_tokens(str_arr);
+		if (isatty(STDIN_FILENO))
+		{
+			printf("%s", user_prompt);
+			fflush(stdout);
+		}
 	}
 	free(lineptr);
 	return (0);
